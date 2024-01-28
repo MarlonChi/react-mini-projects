f3385db8ffcf86f3452de4d7abeb56a6943a436e
MarlonChi <marlonchiodelli@hotmail.com>
2024-01-27 21:16:07 -0300

    feat: sortable table

---
+++ sortable-table/src/components/SearchBar.jsx
@@ -0,0 +1,13 @@
+const SearchBar = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <input
+      className="search-bar"
+      type="text"
+      placeholder="Pesquisar..."
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+    />
+  );
+};
+
+export default SearchBar;
---
+++ sortable-table/src/components/TableHeader.jsx
@@ -0,0 +1,13 @@
+const TableHeader = ({ onColumnClick }) => {
+  return (
+    <thead>
+      <tr>
+        <th onClick={() => onColumnClick("nome")}>Nome</th>
+        <th onClick={() => onColumnClick("idade")}>Idade</th>
+        <th onClick={() => onColumnClick("cargo")}>Cargo</th>
+      </tr>
+    </thead>
+  );
+};
+
+export default TableHeader;
